031d97fd7a6bfa377f8fe0858f313394628ad97e
Camila <mariacamilaarciniegaspua@gmail.com>
2023-09-20 21:19:00 -0500

    capitulo 6: Modelos, templates y vistas (slug, friendly urls, most viewed and liked categories and pages)

--- populate_rango.py
+++ populate_rango.py
@@ -9,41 +9,50 @@ django.setup()
 
 from rango.models import Category, Page
 
+from random import randint
 
 def populate():
     python_pages = [
         {
             'title':'Official Python Tutorial',
-            'url':'http://docs.python.org/3/tutorial/'
+            'url':'http://docs.python.org/3/tutorial/',
+            'views': randint(1, 35)
         }, {
             'title':'How to Think like a Computer Scientist',
-            'url':'http://www.greenteapress.com/thinkpython/'
+            'url':'http://www.greenteapress.com/thinkpython/',
+            'views': randint(1, 35)
         }, {
             'title':'Learn Python in 10 minutes',
-            'url':'http://www.korokithakis.net/tutorials/python/'
+            'url':'http://www.korokithakis.net/tutorials/python/',
+            'views': randint(1, 35)
         } ]
 
 
     django_pages = [
         {
             'title':'Official Django Tutorial',
-            'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial101/'
+            'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial101/',
+            'views': randint(1, 35)
         }, {
             'title':'Django Rocks',
-            'url':'http://www.djangorocks.com/'
+            'url':'http://www.djangorocks.com/',
+            'views': randint(1, 35)
         }, {
             'title':'How to Tango with Django',
-            'url':'http://www.tangowithdjango.com/'
+            'url':'http://www.tangowithdjango.com/',
+            'views': randint(1, 35)
         } ]
 
 
     other_pages = [
         {
             'title':'Bottle',
-            'url':'http://bottlepy.org/docs/dev/'
+            'url':'http://bottlepy.org/docs/dev/',
+            'views': randint(1, 35)
         }, {
             'title':'Flask',
-            'url':'http://flask.pocoo.org'
+            'url':'http://flask.pocoo.org',
+            'views': randint(1, 35)
         } ]
 
 
@@ -58,20 +67,19 @@ def populate():
         c = add_cat(cat)
 
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
 
-    update_views_and_likes('Python',128, 64)
+    update_cat_views_and_likes('Python',128, 64)
 
-    update_views_and_likes('Django',64, 32)
+    update_cat_views_and_likes('Django',64, 32)
 
-    update_views_and_likes('Other Frameworks',32, 16)
-
+    update_cat_views_and_likes('Other Frameworks',32, 16)
 
     for c in Category.objects.all():
         print(f'Category: {c}\n\tViews: {c.views}\tLikes: {c.likes}')
         for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
+            print(f'- {c}: {p} views: {p.views}')
 
 
 def add_page(cat, title, url, views=0):
@@ -88,10 +96,16 @@ def add_cat(name):
     return c
 
 
-def update_views_and_likes(object, views, likes):
+def update_cat_views_and_likes(object, views, likes):
     o = Category.objects.get(name=object)
     o.views = views
     o.likes = likes
+    o.save()
+
+
+def update_page_views(cat, page, views):
+    o = Page.objects.get(category=cat, title=page)
+    o.views = views
     o.save()
 
 
--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args,**kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -12,10 +18,15 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
-    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title       = models.CharField(max_length=128)
-    url         = models.URLField()
-    views       = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Page, self).save(*args,**kwargs)
 
     def __str__(self) -> str:
         return self.title
